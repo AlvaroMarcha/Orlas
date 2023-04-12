7e85a617ca696089601d8c2ceaddd2cf8827c55c
Carlos <122604975+shyshade@users.noreply.github.com>
2023-04-12 18:00:38 +0200

    boton login

---
+++ scripts/scriptlogin.js
@@ -0,0 +1,28 @@
+/*   no se como se hace    */
+
+
+
+function loguear() {
+    
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+
+
+    if(username=="administrador" && password=="1234")
+    {
+
+        window.location="index.html";
+        alert("olee")
+    }
+
+    else {
+
+        alert("jaja fallastes");
+
+    }
+
+}
+
+
+
+
